fc8aac72c49570eeca5037ad7123c9c40d384142
rkuate <45904141+rkuate@users.noreply.github.com>
2020-08-04 06:55:47 +0200

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,30 +1,69 @@
-// Select color input
+// Get color input element
+const colorElement = document.body.querySelector("#colorPicker");
 
-//Select the form element
+//Get the form element
 const formElement = document.body.querySelector("#sizePicker");
 
-// When size is submitted by the user, call makeGrid()
-//console.log(colorValue);
-function makeGrid() {
-  const colorValue = document.body.querySelector("#colorPicker").value;
+//Get the table element
+const tableElement = document.body.querySelector("table");
+
+//Check whether to submit the form ot reset the form
+var reset = false;
+
+/*
+* @description Build the grid
+* @constructor
+* @param {object} colorElement - The color input element
+* @param {object} tableElement - The table element
+*/
+function makeGrid(colorElement , tableElement) {
   // Select size input
   const heightValue = document.body.querySelector("#inputHeight").value;
   const widthValue = document.body.querySelector("#inputWidth").value;
-  //Get the table element
-  const tableElement = document.body.querySelector("table");
+
   for (let rows = 1 ; rows <= heightValue ; rows++){
-    console.log(rows);
+    //Create a <tr> element
     const rowElement = document.createElement("tr");
     tableElement.appendChild(rowElement);
     for (let columns = 1 ; columns <= widthValue ; columns++){
-      console.log(rows);
-      console.log(columns);
+      //Create a <td>
       const columnElement = document.createElement("td");
-      columnElement.style.color = "colorValue";
+      columnElement.style.backgroundColor = colorElement.value;
       rowElement.appendChild(columnElement);
     }
   }
 }
 
-formElement.addEventListener("submit" , makeGrid());
-formElement.removeEventListener("submit" , makeGrid());
+/*
+* @description Change the color of an element
+* @constructor
+* @param {object} colorElement - The color input element
+* @param {object} elementToModify - The element which has been clicked
+*/
+function changeColor(colorElement , elementToModify){
+  elementToModify.style.backgroundColor = colorElement.value;
+}
+
+//Make a grid when the submit button is clicked
+formElement.addEventListener("submit" , function(event){
+  event.preventDefault();
+  if (!reset){
+    makeGrid(colorElement, tableElement);
+    reset = !reset;
+  }else{
+    // Collect all the <td> elements
+    const tableElements = document.body.querySelectorAll('td');
+    for (let element = 0 ; element < tableElements.length ; element++ ){
+      tableElements[element].style.backgroundColor = "#FFFFFF";
+    }
+    colorElement.value = "#FFFFFF";
+  }
+
+});
+
+//Change the color af an element
+tableElement.addEventListener("click" , function(event){
+  //Get the element which fires the event
+  const elementToModify = event.target;
+  changeColor(colorElement, elementToModify);
+})
